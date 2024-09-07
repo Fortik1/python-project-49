a37005a87f095c65caab17d5014abff15ee35527
Даниил <daniil@MacBook-Pro-konstantin-2.local>
2024-09-08 00:36:02 +0300

    step 3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
+from ..cli import welcome_user
+
+
 def main():
     print("Welcome to the Brain Games!")
+    name = welcome_user()
+    print(f"hello, {name}")
+
 
 if __name__ == "__main__":
     main()
